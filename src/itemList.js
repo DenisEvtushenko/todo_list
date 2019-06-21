4a1b26a08da897e44acc9e067abbce47914fd09d
DenisEvtushenko <51827408+DenisEvtushenko@users.noreply.github.com>
2019-06-21 11:58:44 +0300

    Add files via upload

---
+++ src/itemList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+class ItemList extends React.Component {
+    render() {
+        const newItem = this.props.data.map(function(item){
+            return (
+                <div key={item.id} status={item.status}>
+                   <p>{item.text}</p>
+                </div>
+            )    
+        })
+        return (
+               <div>{newItem}</div>
+               )
+    }
+}
+export default ItemList
